cbedcc77967814aafd807d70f644f27c33d5427e
Amos <amos2580@hotmail.co.uk>
2023-05-15 16:22:04 +0100

    Added Share Size to Webfront Added Total Download Speed Better represented filename on hover Filename length is determined by breakpoint Removed row wrapping

--- PlexFront/Utilities/ExtensionMethods.cs
+++ PlexFront/Utilities/ExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using MudBlazor;
 
 namespace PlexFront.Utilities;
 
@@ -11,13 +12,6 @@ public static class ExtensionMethods
 
     public static TimeSpan SecondsToTime(this long seconds) => TimeSpan.FromSeconds(seconds);
 
-    public static string HumanizeDownloadSpeed(this long bytes)
-    {
-        const int scale = 1024;
-        var bytesToMb = 8 / Math.Pow(scale, 2);
-        return $"{bytes * bytesToMb:F2} Mbps";
-    }
-
     public static async Task<TResponse?> DeserializeHttpResponseContentAsync<TResponse>(
         this HttpResponseMessage response) where TResponse : class
     {
@@ -29,5 +23,39 @@ public static class ExtensionMethods
         if (!response.IsSuccessStatusCode) return null;
         var json = await response.Content.ReadAsStringAsync();
         return string.IsNullOrEmpty(json) ? null : JsonSerializer.Deserialize<TResponse>(json, jsonSerializerOptions);
+    }
+
+    public static string TruncateWithEllipsis(this string input, Breakpoint breakpoint)
+    {
+        if (string.IsNullOrEmpty(input)) return input;
+
+        var characterLength = breakpoint switch
+        {
+            Breakpoint.Xs => 24,
+            Breakpoint.Sm => 16,
+            Breakpoint.Md => 24,
+            Breakpoint.Lg => 32,
+            Breakpoint.Xl => 40,
+            Breakpoint.Xxl => 48,
+            _ => 24
+        };
+
+        return input.Length <= characterLength ? input : input[..(characterLength - 3)] + "...";
+    }
+
+    public static List<string> GetSplitLines(this string input)
+    {
+        var result = new List<string>();
+        var index = 0;
+        const int maxLineLength = 42;
+        while (index < input.Length)
+        {
+            result.Add(index + maxLineLength < input.Length
+                ? input.Substring(index, maxLineLength)
+                : input[index..]);
+            index += maxLineLength;
+        }
+
+        return result;
     }
 }
--- PlexFront/Utilities/Utilities.cs
+++ PlexFront/Utilities/Utilities.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace PlexFront.Utilities;
 
@@ -7,5 +9,46 @@ public class Utilities
     public static string GetVersionAsString()
     {
         return Assembly.GetCallingAssembly().GetName().Version?.ToString() ?? "Unknown";
+    }
+
+    public static async Task<(double Total, double Used)?> GetUsedStoragePercentage()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) return null;
+
+        try
+        {
+            using var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = "-c \"df\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            var output = await process.StandardOutput.ReadToEndAsync();
+            await process.WaitForExitAsync();
+            
+            var lines = output.Split('\n');
+            foreach (var line in lines)
+            {
+                if (!line.Contains("/media")) continue;
+
+                var parts = line.Split(new[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+                var total = double.Parse(parts[1]);
+                var used = double.Parse(parts[2]);
+                return (total, used);
+            }
+        }
+        catch
+        {
+            return null;
+        }
+
+        return null;
     }
 }
